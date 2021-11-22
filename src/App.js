7a41b7ed27759291245dd298402b5ae95ad2fb97
Spencer Whiteman <swvisuals@gmail.com>
2021-11-21 21:42:53 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from './Components/Googlepay.button';
+
+const App = () => {
+  return (
+   <div className="buttonlayout">
+    
+   </div>
+  );
+}
+
+
+export default App;
---
+++ src/Components/Googlepay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Googlepay = () => {
+    return (
+        <div className="Googlepay">
+           
+        </div>
+    );
+};
+
+export default Googlepay;
