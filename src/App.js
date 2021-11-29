be22cfdc85a05d3088ec8917836e38d875a1eb0e
abc <abc@example.com>
2021-11-28 23:17:15 -0500

    logo and material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button } from './Components/Googlepay.button';
+import Header from './Components/Header';
 
 const App = () => {
   return (
    <div className="buttonlayout">
-    
+    <Header />
    </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Img1 from '../Images/No cinematographer or visuals copy.png';
+import styled from 'styled-components';
+
+const Header = () => {
+    return (
+        <div>
+            <img src={Img1} alt="Logo" />
+        </div>
+    )
+}
+
+
+
+
+export default Header
